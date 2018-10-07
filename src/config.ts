bc06bbfc29a33d1d431d27184bb59f57c56921b1
Leonard LEUNG CHONG WO <leungc_l@etna-alternance.net>
2018-10-07 17:55:27 +0200

    first commit

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const FirebaseConfig = {
+    fire : {
+        apiKey: "AIzaSyBoGc3JuyX0NQRnCW-LqlsIwjZloqltd24",
+        authDomain: "mon-agenda-f3718.firebaseapp.com",
+        databaseURL: "https://mon-agenda-f3718.firebaseio.com",
+        projectId: "mon-agenda-f3718",
+        storageBucket: "mon-agenda-f3718.appspot.com",
+        messagingSenderId: "329653030004"
+    }
+}
