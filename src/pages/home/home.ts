0fa12e0786277aaef5912276a0e78bc73b9be457
Leonard LEUNG CHONG WO <leungc_l@etna-alternance.net>
2018-10-23 17:42:16 +0200

    filtrer par groupe et couleurs de l'app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, ModalController, ToastController } from 'ionic-angular';
 import { EventModalPage } from '../events/event-modal';
 import { EventsService } from '../../services/events.service';
+import { GroupsService } from '../../services/groups.service';
 
 @Component({
     selector: 'page-home',
@@ -12,17 +13,19 @@ export class HomePage {
     eventSource = [];
     viewTitle: string;
     selectedDay = new Date();
-
+    selected_group: any;
     calendar = {
         mode: 'month',
         currentDate: new Date()
     };
+    groups: any;
 
     constructor(
         public navCtrl: NavController,
         private modalCtrl: ModalController,
         public eventsService: EventsService,
-        public toast: ToastController
+        public toast: ToastController,
+        public groupsService: GroupsService
     ) { }
 
     addEvent() {
@@ -38,19 +41,41 @@ export class HomePage {
     }
 
     ionViewWillEnter() {
-        this.refreshEvents();
-    }
-
-    refreshEvents() {
-        this.eventsService.getMyEvents().then(res => {
-            this.eventSource = res;
+        this.selected_group = 'Tous les groupes';
+        this.groupsService.getMyGroups().then(res => {
+            this.groups = res;
         }, err => {
             this.toast.create({
-                message: 'Erreur lors de la récupération des événements',
+                message: 'Erreur lors de la récupération des groupes',
                 duration: 5000,
                 position: 'bottom'
             }).present();
         });
+        this.refreshEvents();
+    }
+
+    refreshEvents() {
+        if (this.selected_group === 'Tous les groupes') {
+            this.eventsService.getMyEvents().then(res => {
+                this.eventSource = res;
+            }, err => {
+                this.toast.create({
+                    message: 'Erreur lors de la récupération des événements',
+                    duration: 5000,
+                    position: 'bottom'
+                }).present();
+            });
+        } else {
+            this.eventsService.getEventsByGroup(this.selected_group).then(res => {
+                this.eventSource = res;
+            }, err => {
+                this.toast.create({
+                    message: 'Erreur lors de la récupération des événements',
+                    duration: 5000,
+                    position: 'bottom'
+                }).present();
+            });
+        }
     }
 
     onEventSelected(evnt) {
